f26595403fd1f47e6cf96473595bc2a71022c1c9
Michael Beemer <michael.beemer@dynatrace.com>
2019-07-24 11:33:14 -0400

    Updated body parser to support cloudevents

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: true,
+    bodyParser: false,
   });
+  app.use(json({ type: ['application/json', 'application/cloudevents+json'] }));
   await app.listen(3000);
 }
 bootstrap();
